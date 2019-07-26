6735f50bf548afe712b1247d87e41dadafec2346
FilAlves <38048444+FilAlves@users.noreply.github.com>
2019-07-26 23:42:50 +0100

    Update install.py

--- install.py
+++ install.py
@@ -1,10 +1,6 @@
 import os
 
 os.system("sudo apt-get install cutadapt")
-os.system("sudo apt-get install build-essential")
-os.system("sudo apt-get install zlib1g")
-os.system("sudo apt-get install zlib1g-dev")
-os.system("sudo apt-get install default-jre")
 
 os.system("sed -i -e 's/CC_OPTS    = -g -Wall -D__USE_FIXED_PROTOTYPES__/CC_OPTS = -g -Wall -D__USE_FIXED_PROTOTYPES_ -fpermissive/' software/primer3/src/Makefile")
 
